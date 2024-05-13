0f8f4d1f709e448f6532715f3e43aa87ecf51378
naylatanjung <naylatanjung79@gmail.com>
2024-05-13 20:19:43 +0700

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,21 @@
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <p>Copyright © 2036 <a href="#">Cyborg Gaming</a> Company. All rights reserved.</p>
+      </div>
+    </div>
+  </div>
+</footer>
+
+
+<!-- Scripts -->
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+<script src="assets/js/isotope.min.js"></script>
+<script src="assets/js/owl-carousel.js"></script>
+<script src="assets/js/tabs.js"></script>
+<script src="assets/js/popup.js"></script>
+<script src="assets/js/custom.js"></script>
