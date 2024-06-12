b47a477d698691ab2750cb08f101d689b3960a15
naylatanjung <naylatanjung79@gmail.com>
2024-06-12 22:08:10 +0700

    new banner & logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,14 +8,6 @@
 
                     <!-- ***** Banner Start ***** -->
                     <div class="main-banner">
-                        <div class="row">
-                            <div class="col-lg-7">
-                                <div class="header-text">
-                                    <h6>Welcome To Cyborg</h6>
-                                    <h4><em>Browse</em> Our Popular Games Here</h4>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                     <!-- ***** Banner End ***** -->
 
